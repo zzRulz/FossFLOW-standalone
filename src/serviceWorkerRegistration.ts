04b6ecac8cd4b662c61429c149ae8aa8fb25f179
muziyu <2785745642@qq.com>
2025-07-07 15:25:16 +0800

    switch create-react-app to rsbuild

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -13,13 +13,13 @@ type Config = {
 
 export function register(config?: Config) {
   if ('serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(import.meta.env.ASSET_PREFIX, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       return;
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.ASSET_PREFIX}/service-worker.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
