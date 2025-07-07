07d4f2f646ce14d781b2651308825fa9d814d818
mu ziyu <61375428+AmazingRain@users.noreply.github.com>
2025-07-08 02:10:06 +0800

    switch create-react-app to rsbuild (#36)
    All verified working, thanks for your efforts, much appreciated!
    Stan

---
+++ rsbuild.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+export default defineConfig({
+    plugins: [pluginReact()],
+    output: {
+        distPath: {
+            root: 'build',
+        },
+        // https://rsbuild.rs/guide/advanced/browser-compatibility
+        polyfill: 'usage',
+    }
+});
